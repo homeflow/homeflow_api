44c4649f2a0c7f566a8f29a0c562748760779e22
Louis Bacon <louis@Louiss-MacBook-Pro.local>
2012-06-11 17:49:26 +0100

    Another fix - ill get the hang of this soon

--- lib/homeflow/api/site_page.rb
+++ lib/homeflow/api/site_page.rb
@@ -4,7 +4,7 @@ module Homeflow
 
     is_resource :site_pages
 
-    def find_by_key_and_site_id(key, site_id)
+    def self.find_by_key_and_site_id(key, site_id)
     	Request.run_for(Homeflow::API::ResourceIdentifier.new("/sites/#{site_id}/pages/#{key}"))
     end
 
