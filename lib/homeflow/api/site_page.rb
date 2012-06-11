e9a47f9e238eb31fed6c5d726d01ba0940030033
Louis Bacon <louis@Louiss-MacBook-Pro.local>
2012-06-11 17:45:29 +0100

    Changed site page

--- lib/homeflow/api/site_page.rb
+++ lib/homeflow/api/site_page.rb
@@ -4,8 +4,8 @@ module Homeflow
 
     is_resource :site_pages
 
-    def find(id, site_id)
-    	Request.run_for(Homeflow::API::ResourceIdentifier.new("/sites/#{site_id}/pages/#{id}"))
+    def find_by_key_and_site_id(key, site_id)
+    	Request.run_for(Homeflow::API::ResourceIdentifier.new("/sites/#{site_id}/pages/#{key}"))
     end
 
    end
