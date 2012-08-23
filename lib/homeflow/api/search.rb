1d774b285a05970d3f923b22f414228478d0ce5e
Daniel Cooper <daniel@14lines.com>
2012-08-23 11:44:50 +0100

    Can now create searches

--- lib/homeflow/api/search.rb
+++ lib/homeflow/api/search.rb
@@ -4,6 +4,10 @@ module Homeflow
 
     is_resource :searches
 
+    def self.create(params)
+      Request.run_for(Homeflow::API::Post.new("/#{resource_uri}/", {}, params))
+    end
+
 
    end
  end
