527bbdcfe57b872701294248cf0a2d2f588d64a5
Daniel Cooper <daniel@14lines.com>
2013-01-25 12:36:36 +0000

    Use password_resets resource

--- lib/homeflow/api/password_reset.rb
+++ lib/homeflow/api/password_reset.rb
@@ -2,7 +2,7 @@ module Homeflow
   module API
    class PasswordReset < Resource
 
-    is_resource :leads
+    is_resource :password_resets
 
     def self.create(params)
       Request.run_for(Homeflow::API::Post.new("/#{resource_uri}/", {}, params))
