dbf484fd3bcfd7d9956449ec1abf8f20d8ff10e9
Louis Bacon <louis@homeflow.co.uk>
2013-04-18 13:36:02 +0100

    Update Search tweaks to Gem

--- lib/homeflow/api/search.rb
+++ lib/homeflow/api/search.rb
@@ -8,8 +8,8 @@ module Homeflow
       Request.run_for(Homeflow::API::Post.new("/#{resource_uri}/", {}, params))
     end
 
-    def self.update
-    	Request.run_for(Homeflow::API::Put.new("/#{resource_uri}/", {}, params))
+    def self.update(id, params)
+    	Request.run_for(Homeflow::API::Put.new("/#{resource_uri}/#{id}", {}, params))
     end
 
    end
