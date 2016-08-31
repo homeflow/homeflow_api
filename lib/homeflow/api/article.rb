deb6f082685caff4530a07f916e5a806446d6ddb
Louis Bacon <louis@homeflow.co.uk>
2016-08-31 17:09:32 +0100

    find by site_id and id

--- lib/homeflow/api/article.rb
+++ lib/homeflow/api/article.rb
@@ -4,6 +4,10 @@ module Homeflow
 
     is_resource :articles
 
+    def self.find_by_site_id_and_article_id(site_id, article_id)
+      Request.run_for(Homeflow::API::ResourceIdentifier.new("/sites/#{site_id}/articles/#{article_id}"))
+    end
+
    end
  end
 end
