6a7248e450ee239ca29287767566617a23e114a9
Louis Bacon <louis@homeflow.co.uk>
2017-07-13 15:13:31 +0100

    Country location finder

--- lib/homeflow/api/location.rb
+++ lib/homeflow/api/location.rb
@@ -14,6 +14,10 @@ module Homeflow
       Request.run_for(Homeflow::API::ResourceIdentifier.new("/counties/#{county_id}/locations/#{location_id}"))
     end
 
+    def self.find_by_country_id_and_county_id_and_location_id(county_id, location_id)
+      Request.run_for(Homeflow::API::ResourceIdentifier.new("/countries/#{country_id}/counties/#{county_id}/locations/#{location_id}"))
+    end
+
    end
  end
 end
