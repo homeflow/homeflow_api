bcf84f6180e21eeb98425718307396dda6a836d0
Louis Bacon <louis@homeflow.co.uk>
2017-07-13 15:21:49 +0100

    Minor fix

--- lib/homeflow/api/location.rb
+++ lib/homeflow/api/location.rb
@@ -14,7 +14,7 @@ module Homeflow
       Request.run_for(Homeflow::API::ResourceIdentifier.new("/counties/#{county_id}/locations/#{location_id}"))
     end
 
-    def self.find_by_country_id_and_county_id_and_location_id(county_id, location_id)
+    def self.find_by_country_id_and_county_id_and_location_id(country_id, county_id, location_id)
       Request.run_for(Homeflow::API::ResourceIdentifier.new("/countries/#{country_id}/counties/#{county_id}/locations/#{location_id}"))
     end
 
