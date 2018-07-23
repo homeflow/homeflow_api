0a76e7787edf82f04a5c593c4c5b1f21c3ba7a51
Daniel Whitston <daniel.whitston@homeflow.co.uk>
2018-07-23 13:30:40 +0100

    Unbreak the require list
    https://github.com/homeflow/ctesius/issues/11323

--- lib/homeflow/api.rb
+++ lib/homeflow/api.rb
@@ -43,8 +43,8 @@ require current_dir + '/api/node'
 require current_dir + '/api/message'
 require current_dir + "/api/favourite_property"
 require current_dir + "/api/rda_agency_preference"
-require current_dir + "/api/response"
 require current_dir + "/api/rda_user_preference"
+require current_dir + "/api/response" # THIS MUST ALWAYS BE LAST IN THE LIST
 
 module Homeflow
   module API
